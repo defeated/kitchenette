d746764a4dbb819ff5a57834116a5e51a2a304d1
eddie cianci <defeated2k4@gmail.com>
2013-08-10 13:43:19 -0400

    initial commit
    bundle install --path vendor
    bundle package

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kitchenette::Application.config.secret_key_base = '15b4285ff04635eba254bc39f55c4a4b636feee5a11d52a4cbc20e7162619b90330d42b874099dc84751dadb35dc979d3e701df3c73f9f1ac5b954c845a57bfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kitchenette::Application.config.session_store :cookie_store, key: '_kitchenette_session'
