775cce33e3f8740b692ad3e3d08d7f999519197e
eddie cianci <defeated2k4@gmail.com>
2013-08-11 00:50:11 -0400

    display user gravatar in nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+require 'digest/md5'
+
 module ApplicationHelper
+  def gravatar_url(email, options = {})
+    hash      = Digest::MD5.hexdigest email.to_s.downcase.strip
+    defaults  = { d: 'mm', r: 'r' }
+    options   = defaults.merge options
+
+    "https://secure.gravatar.com/avatar/#{ hash }.jpg?#{ options.to_query }"
+  end
+
+  def gravatar_image(email, size = 48)
+    image_tag gravatar_url(email, s: size),
+      height: size, width: size, alt: 'avatar', class: 'avatar'
+  end
 end
