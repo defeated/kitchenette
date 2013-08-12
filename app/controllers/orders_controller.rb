943f962fdbdefbc7ae680bfd906963f0015adbac
eddie cianci <defeated2k4@gmail.com>
2013-08-11 22:09:56 -0400

    change design for date of the order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,36 +16,31 @@ class OrdersController < ApplicationController
 
     @roster = [
       {
-        day: 'Monday',
-        date: 'Aug 12',
+        date: Time.zone.local(2013, 8, 12),
         restaurant: 'Panera',
         url: 'http://panera.com',
         cuisine: 'cafe, bakery'
       },
       {
-        day: 'Tuesday',
-        date: 'Aug 13',
+        date: Time.zone.local(2013, 8, 13),
         restaurant: 'Au Bon Pain',
         url: 'http://aubonpain.com',
         cuisine: 'cafe, bakery'
       },
       {
-        day: 'Wednesday',
-        date: 'Aug 14',
+        date: Time.zone.local(2013, 8, 14),
         restaurant: 'S&S Restaurant',
         url: 'http://www.sandsrestaurant.com/',
         cuisine: 'deli'
       },
       {
-        day: 'Thursday',
-        date: 'Aug 15',
+        date: Time.zone.local(2013, 8, 15),
         restaurant: 'Flour Bakery',
         url: 'http://flourbakery.com/',
         cuisine: 'cafe, bakery'
       },
       {
-        day: 'Friday',
-        date: 'Aug 16',
+        date: Time.zone.local(2013, 8, 16),
         restaurant: 'Chipotle',
         url: 'http://chipotle.com',
         cuisine: 'mexican'
