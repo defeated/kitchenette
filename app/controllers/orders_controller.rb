eb6ef097775622e08583dac79760543e91267a0f
eddie cianci <defeated2k4@gmail.com>
2013-08-11 22:09:22 -0400

    on "this week", display cuisine next to restaurant name

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,31 +19,36 @@ class OrdersController < ApplicationController
         day: 'Monday',
         date: 'Aug 12',
         restaurant: 'Panera',
-        url: 'http://panera.com'
+        url: 'http://panera.com',
+        cuisine: 'cafe, bakery'
       },
       {
         day: 'Tuesday',
         date: 'Aug 13',
         restaurant: 'Au Bon Pain',
-        url: 'http://aubonpain.com'
+        url: 'http://aubonpain.com',
+        cuisine: 'cafe, bakery'
       },
       {
         day: 'Wednesday',
         date: 'Aug 14',
         restaurant: 'S&S Restaurant',
-        url: 'http://www.sandsrestaurant.com/'
+        url: 'http://www.sandsrestaurant.com/',
+        cuisine: 'deli'
       },
       {
         day: 'Thursday',
         date: 'Aug 15',
         restaurant: 'Flour Bakery',
-        url: 'http://flourbakery.com/'
+        url: 'http://flourbakery.com/',
+        cuisine: 'cafe, bakery'
       },
       {
         day: 'Friday',
         date: 'Aug 16',
         restaurant: 'Chipotle',
-        url: 'http://chipotle.com'
+        url: 'http://chipotle.com',
+        cuisine: 'mexican'
       }
     ]
 
